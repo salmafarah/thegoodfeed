2f4e7df8853a8ce6190b332c9201a0d0e1e5db15
Salma Farah <salmafarah08@gmail.com>
2020-07-16 18:51:20 -0400

    added icon img on browser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        Landing Page
+      </div>
+    )
+  }
 }
+
+
+
+
 
 export default App;

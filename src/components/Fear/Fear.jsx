8899c7de660c7ab6e67ce591f1798f0a1c4a5519
Salma Farah <salmafarah08@gmail.com>
2020-07-18 23:03:20 -0400

    fix footer and nav

--- src/components/Fear/Fear.jsx
+++ src/components/Fear/Fear.jsx
@@ -4,86 +4,244 @@ import './Fear.css';
 class Fear extends Component {
     render(){
         return(
-          <>
-            <div className=" container align-items-center Margin">
-              <div className="row mb-5">
-                      <div className="col sm-4 ">
-                        <div className="card img-fluid">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
+            <div className="container align-items-center mt-5">
+              <div className="row mb-4">
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col sm-4">
-                        <div className="card Card-Size img-fluid">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col sm-4">
-                        <div className="card">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col sm-4">
-                        <div className="card">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                  </div>
-                  <div className="row mb-5">
-                      <div className="col sm-4 ">
-                        <div className="card img-fluid">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                      <div className="col sm-4">
-                        <div className="card Card-Size img-fluid">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                      <div className="col sm-4">
-                        <div className="card">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                      <div className="col sm-4">
-                        <div className="card">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                  </div>
-                  <div className="row mb-5">
-                      <div className="col sm-4 ">
-                        <div className="card img-fluid">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                      <div className="col sm-4">
-                        <div className="card Card-Size img-fluid">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                      <div className="col sm-4">
-                        <div className="card">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
-                        </div>
-                      </div>
-                      <div className="col sm-4">
-                        <div className="card">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top"/>
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                   </div>
 
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
 
 
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
 
 
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
 
 
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
 
 
-              </div>
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
 
-</>
+
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col sm-4 ">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col sm-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+            </div>    
+            
         );
     }
 }

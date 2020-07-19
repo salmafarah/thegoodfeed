f503c440347cb71a028e42ea0781203123b3f079
Salma Farah <salmafarah08@gmail.com>
2020-07-19 19:45:08 -0400

    adding to grid

--- src/components/Fear/Fear.jsx
+++ src/components/Fear/Fear.jsx
@@ -9,17 +9,55 @@ class Fear extends Component {
               <div className="row mb-4">
                       <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded Big"/>
+                        <img src="https://imgur.com/xrVd1He.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        <Iframe url="https://www.youtube.com/embed//KlUMrzwmbyo" className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                          <Iframe url="https://www.youtube.com/embed/9TpJmvgqJyY"  className="card-img-top rounded"/>
+                          <img src="https://imgur.com/Tp9bkYW.jpg" alt="..." className="card-img-top rounded"/>
+
+                        </div>
+                      </div>
+                  </div>
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/5t4Amkv.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/t3TqLIM.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                    </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <Iframe url="https://www.youtube.com/embed/I9NOmtBU-uw" className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/mQ9Oecs.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/cvEQeGH.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                   </div>
@@ -37,44 +75,7 @@ class Fear extends Component {
                       </div>
                       <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
-                        </div>
-                      </div>
-                    </div>
-
-
-                  <div className="row mb-4">
-                      <div className="col xl-4">
-                        <div className="card img-fluid border border-white rounded">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
-                        </div>
-                      </div>
-                      <div className="col xl-4">
-                        <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
-                        </div>
-                      </div>
-                      <div className="col xl-4">
-                        <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
-                        </div>
-                      </div>
-                  </div>
-
-                  <div className="row mb-4">
-                      <div className="col xl-4">
-                        <div className="card img-fluid border border-white rounded">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
-                        </div>
-                      </div>
-                      <div className="col xl-4">
-                        <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
-                        </div>
-                      </div>
-                      <div className="col xl-4">
-                        <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        <img src="https://imgur.com/Q0zrhpe.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                     </div>
@@ -87,7 +88,7 @@ class Fear extends Component {
                       </div>
                       <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        <img src="https://imgur.com/NIzHIN6.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col xl-4">
@@ -120,7 +121,7 @@ class Fear extends Component {
                   <div className="row mb-4">
                   <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                          <img src="https://imgur.com/5mFa3Jv.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col xl-4">
@@ -144,7 +145,7 @@ class Fear extends Component {
                       </div>
                       <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                        <img src="https://imgur.com/1Wke4D1.jpg" alt="..." className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col xl-4">
@@ -187,7 +188,7 @@ class Fear extends Component {
                       </div>
                       <div className="col xl-4">
                         <div className="card img-fluid border border-white rounded">
-                        <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top  rounded"/>
+                        <img src="https://imgur.com/OSdOrXz.jpg" alt="..." className="card-img-top  rounded"/>
                         </div>
                       </div>
                   </div>

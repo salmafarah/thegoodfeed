dfa6577018a15da8160786b20333ef20c0c1dc30
Hiba Codes <53172879+hibacodes@users.noreply.github.com>
2020-07-22 11:03:11 -0700

    Add Sad Page

---
+++ src/components/Sad/Sad.jsx
@@ -0,0 +1,215 @@
+import React, {Component} from 'react';
+import '../EmotionCSS.css';
+import Iframe from 'react-iframe'
+
+class Sad extends Component {
+    render(){
+        return(
+            <div className="container align-items-center mt-5">
+              <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/Dx7O0AP.jpg></iframe>" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <Iframe url="https://youtube.com/embed/PRJHORk4pZg" className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/pCu09GI.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                          <a href="https://www.goodreads.com/book/show/40880320-don-t-be-sad" target="_blank" rel="noopener noreferrer"> 
+                          <img src="https://imgur.com/nB2BqRI.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                      
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/3lrAI6u.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                    <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://www.goodreads.com/book/show/16112413-reclaim-your-heart" target="_blank" rel="noopener noreferrer"> 
+                        <img src="https://imgur.com/OwngtC6.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                      
+                        </div>
+                      </div>
+                    </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/0ugBQDD.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://deenspiration.com/podcast-player/" target="_blank" rel="noopener noreferrer">   
+                        <img src="https://imgur.com/lqHOshv.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/BZThqeX.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="http://mentalhealth4muslims.com/dealing-with-grief/" target="_blank" rel="noopener noreferrer">  
+                        <img src="https://imgur.com/145Tbuo.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                       <img src="https://imgur.com/TR1tSZY.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://yaqeeninstitute.org/sarah-sultan/everything-is-falling-apart-dealing-with-unexpected-life-detours/" target="_blank" rel="noopener noreferrer">  
+                        <img src="https://imgur.com/EkK7Kdb.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                    </div>
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/X4nErHM.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <Iframe url="https://youtube.com/embed/Qdkrzb9zwBs" className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/yBB817G.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://www.goodreads.com/book/show/41811277-surahs-verse-from-the-holy-quran-for-stress-depression-anxiety" target="_blank" rel="noopener noreferrer"> 
+                          <img src="https://imgur.com/8qJIRXK.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/wNiiFZa.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://www.goodreads.com/book/show/910495.Du_a" target="_blank" rel="noopener noreferrer">
+                        <img src="https://imgur.com/9V245sj.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                  <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                          <img src="https://imgur.com/UypVEW5.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://www.apa.org/research/action/speaking-of-psychology" target="_blank" rel="noopener noreferrer"> 
+                        <img src="https://imgur.com/0W6PwzU.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/oly4jDm.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://thriveglobal.com/stories/stop-social-media-comparison-tips/" target="_blank" rel="noopener noreferrer"> 
+                        <img src="https://imgur.com/U03LLyL.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/OJfHHYi.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://www.psychologytoday.com/us/blog/compassion-matters/201507/the-value-sadness" target="_blank" rel="noopener noreferrer">  
+                        <img src="https://imgur.com/P2JIH0g.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/Xns1Gi1.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <Iframe url="https://youtube.com/embed/eFbBBQPgkYk" className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/KIX0Txk.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://www.goodreads.com/book/show/21413760-the-upward-spiral" target="_blank" rel="noopener noreferrer">
+                        <img src="https://imgur.com/hRENN3A.jpg" alt="..." className="card-img-top rounded Big"/></a>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <img src="https://imgur.com/x5i9UTH.jpg" alt="..." className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                        <a href="https://www.goodreads.com/book/show/13237327-the-magic" target="_blank" rel="noopener noreferrer">  
+                        <img src="https://imgur.com/8TAJrBY.jpg" alt="..." className="card-img-top  rounded Big"/></a>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+          
+            
+        );
+    }
+}
+
+export default Sad;

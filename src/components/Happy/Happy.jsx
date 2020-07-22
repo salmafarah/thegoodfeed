371b9b2bb96be93c02b74fb22ea180c911984b9a
Alaa Salim <Al@alaas-mbp.lan>
2020-07-22 01:30:57 -0700

    Happy page is done

---
+++ src/components/Happy/Happy.jsx
@@ -0,0 +1,324 @@
+import React, {Component} from 'react';
+import Iframe from 'react-iframe';
+
+
+class Anger extends Component {
+    render(){
+        return(
+            <div className="container align-items-center mt-5">
+              <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://www.amazon.com/Its-All-Your-Head-Happiness/dp/0060760001" 
+                               rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/mR2EYgB.png" 
+                             alt="...." 
+                             className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/4bUU6FM.png" 
+                            alt="Image" 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://gretchenrubin.com/books/the-happiness-project/about-the-book/" 
+                               rel="noopener noreferrer">
+                                <img src="https://i.imgur.com/YinHXT2.png" 
+                                 alt="...." 
+                                 className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://yaqeeninstitute.org/yasienmohamed/the-idea-of-happiness-in-the-quran/" 
+                               rel="noopener noreferrer">
+                                <img src="https://i.imgur.com/6IBvfJ5.png" 
+                                alt="....." 
+                                className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/Ud2txvh.jpg" 
+                            alt="...." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://www.nytimes.com/guides/well/how-to-be-happy" 
+                               rel="noopener noreferrer">
+                                <img src="https://i.imgur.com/wTY5Z5n.png" 
+                                 alt="...." 
+                                 className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+                    </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://gretchenrubin.com/books/better-than-before/about-the-book/" 
+                               rel="noopener noreferrer">
+                                <img src="https://i.imgur.com/CxIUawf.png" 
+                                alt="....." 
+                                className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://www.psychologytoday.com/us/blog/click-here-happiness/201801/how-be-happy-23-ways-be-happier" 
+                            rel="noopener noreferrer">
+                                <img src="https://i.imgur.com/pGZCUO5.png" 
+                                alt="....." 
+                                className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://gretchenrubin.com/books/the-four-tendencies/intro/" 
+                            rel="noopener noreferrer">
+                                <img src="https://i.imgur.com/R8jdHzx.png" 
+                                alt="....." 
+                                className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                    <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/74BUnzo.png" 
+                            alt="....." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <Iframe url="https://www.youtube.com/embed?v=78nsxRxbf4w"  
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      {/* <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/DBKJaXH.png" 
+                            alt="....." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div> */}
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://greatergood.berkeley.edu/podcasts/series/the_science_of_happiness" 
+                            rel="noopener noreferrer">
+                                <img src="https://i.imgur.com/nfbhiQh.png" 
+                                alt="....." 
+                                className="card-img-top rounded Big"/>
+                            </a>
+                        </div>
+                      </div>
+                    </div>
+
+
+                  <div className="row mb-4">
+                  <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <Iframe url="https://www.youtube.com/embed?v=_R6R62qUgIs"  
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/1ilCaZh.png" 
+                            alt="....." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <Iframe url="https://www.youtube.com/embed?v=pIhUUs28dj0"  
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/XO8xPfz.png" 
+                            alt="......." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <Iframe url="https://www.youtube.com/embed?v=FiPoBO6mQ2o"  
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/aNEMuAp.png" 
+                            alt="......." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                  <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <Iframe url="https://www.youtube.com/embed?v=kj-sfwmw9KM" 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+        
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://www.whyislam.org/spiritual-journeys/happiness/" 
+                             rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/x1NTioL.png" 
+                            alt="....." 
+                            className="card-img-top rounded Big"/>
+                        </a>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <Iframe url="https://www.youtube.com/embed?v=g3SO3Ej2G6E" 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                  <div className="col xl-4">
+                    <div className="card img-fluid border border-white rounded">
+                    <a href="https://yaqeeninstitute.org/omar-edward-moad/the-technology-of-happiness/" 
+                    rel="noopener noreferrer">
+                        <img src="https://i.imgur.com/Lx14WLo.png" 
+                        alt="....." 
+                        className="card-img-top rounded Big"/>
+                        </a>
+                    </div>
+                    </div>
+
+                    <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/8FA3ko9.png" 
+                            alt="......" 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                      <div className="card img-fluid border border-white rounded">
+                            <a href="https://gretchenrubin.com/podcasts/" 
+                             rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/IhoG7WQ.jpg" 
+                            alt="....." 
+                            className="card-img-top rounded Big"/>
+                        </a>
+                        </div>
+                      </div>
+                      
+                  </div>
+
+
+                  <div className="row mb-4">
+                  <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/7YmLNQw.png" 
+                            alt="...." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://www.abebooks.com/Happiness-New-Approach-Life-Want-Lyubomirsky/30700043475/bd?cm_mmc=ggl-_-US_Shopp_Trade-_-used-_-naa&gclid=EAIaIQobChMIrJmm76_g6gIVLR-tBh2MZgB1EAQYAiABEgJSSvD_BwE" 
+                             rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/wcBpyhZ.png" 
+                            alt="....." 
+                            className="card-img-top rounded Big"/>
+                        </a>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/8aI8q2o.png" 
+                            alt="...." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+
+
+                  <div className="row mb-4">
+                  <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <a href="https://www.amazon.com/Subtle-Art-Not-Giving-Counterintuitive/dp/0062457713" 
+                             rel="noopener noreferrer">
+                            <img src="https://i.imgur.com/Oltaumx.png" 
+                            alt="....." 
+                            className="card-img-top rounded Big"/>
+                        </a>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/yNzyzK9.png" 
+                            alt="....." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+
+                      <div className="col xl-4">
+                        <div className="card img-fluid border border-white rounded">
+                            <img src="https://i.imgur.com/cwsMZjo.png" 
+                            alt="....." 
+                            className="card-img-top rounded"/>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+        );
+    }
+}
+
+export default Anger;

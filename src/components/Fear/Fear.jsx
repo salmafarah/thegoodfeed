c012c33da28d56cbb1e19e5b415b3b8398ae4109
Salma Farah <salmafarah08@gmail.com>
2020-07-24 20:28:37 -0400

    updated fear

--- src/components/Fear/Fear.jsx
+++ src/components/Fear/Fear.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+
 import '../EmotionCSS.css';
 import Iframe from 'react-iframe'
 import { Link } from 'react-router-dom';
@@ -51,7 +52,7 @@ class Fear extends Component {
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 Big mb-4">
                       <div className="card border border-white rounded">
                         <a href="https://productivemuslim.com/3-powerful-tips-to-overcome-fear/" target="_blank" rel="noopener noreferrer">
-                          <img src="https://imgur.com/xrVd1He.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                          <img src="https://imgur.com/xrVd1He.jpg" alt="3 Powerful Tips to Overcome Fear" className="rounded card-img-top img-fluid"/>
                         </a>
                      </div>
                    
@@ -63,28 +64,27 @@ class Fear extends Component {
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 mb-4">
                       <div className="card border border-white rounded">
-                          <img src="https://imgur.com/Tp9bkYW.jpg" alt="..." className="rounded card-img-top img-fluid" />
+                          <img src="https://imgur.com/zHuh7lZ.jpg" alt="Erica Jong quote" className="rounded card-img-top img-fluid" />
                        </div>
-                    
                       </div>
                 </div>
 
                   <div className="row">
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                          <img src="https://imgur.com/ff8a15Q.jpg" alt="..." className="card-img-top rounded"/>
+                          <img src="https://imgur.com/MiiWxqH.jpg" alt="Islamic quote" className="card-img-top rounded"/>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 Big mb-4">
                         <div className="card border border-white rounded card-img-top1">
                         <a href="https://www.psychologytoday.com/ca/blog/insight-therapy/201009/overcoming-fear-the-only-way-out-is-through" target="_blank" rel="noopener noreferrer">
-                          <img src="https://imgur.com/5t4Amkv.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                          <img src="https://imgur.com/5t4Amkv.jpg" alt="The only way out through" className="rounded card-img-top img-fluid"/>
                         </a>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/t3TqLIM.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/A89pZgu.jpg" alt="Man climbing cliff" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                     </div>
@@ -98,12 +98,12 @@ class Fear extends Component {
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/mQ9Oecs.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/OZFIhoD.jpg" alt="Alfred Adler" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4  mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/iwRxef6.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/iwRxef6.jpg" alt="Lion" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                   </div>
@@ -111,13 +111,13 @@ class Fear extends Component {
                   <div className="row">
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4  mb-4">
                         <div className="card border border-white rounded card-img-top">
-                          <img src="https://imgur.com/Y0ET91g.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                          <img src="https://imgur.com/gDcmBbv.jpg" alt="Indeed, with hardship will be ease" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4  mb-4 Big">
                         <div className="card border border-white rounded card-img-top">
                         <a href="https://www.quranicconnection.tv/fear-in-islam/" target="_blank" rel="noopener noreferrer">
-                          <img src="https://imgur.com/Q0zrhpe.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                          <img src="https://imgur.com/Q0zrhpe.jpg" alt="Fear in Islam" className="rounded card-img-top img-fluid"/>
                         </a>
                         </div>
                       </div>
@@ -132,7 +132,7 @@ class Fear extends Component {
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4 Big">
                         <div className="card border border-white rounded card-img-top">
                         <a href="https://www.soundvision.com/article/10-tips-to-liberate-yourself-from-fear" target="_blank" rel="noopener noreferrer">
-                          <img src="https://imgur.com/NIzHIN6.jpg" alt="..." className="card-img-top rounded"/>
+                          <img src="https://imgur.com/NIzHIN6.jpg" alt="10 Tips to Liberate Yourself From Fear" className="card-img-top rounded"/>
                         </a>
                         </div>
                       </div>
@@ -158,13 +158,13 @@ class Fear extends Component {
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4 Big">
                         <div className="card border border-white rounded card-img-top">
                         <a href="http://www.naseeb.com/journals/seeking-courage-from-allah-195091" target="_blank" rel="noopener noreferrer">
-                          <img src="https://imgur.com/5mFa3Jv.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                          <img src="https://imgur.com/5mFa3Jv.jpg" alt="Seeking Courage From Allah" className="rounded card-img-top img-fluid"/>
                         </a>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/TqKDWcb.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/OMTXDEM.jpg" alt="Courage is resisitance to Fear, Mastery of Fear, Not Absence of Fear" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                   </div>
@@ -173,17 +173,17 @@ class Fear extends Component {
                   <div className="row">
                   <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/Qb2Mjqb.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/HhW4TOn.jpg" alt="Do not lose hope nor be sad" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/11iMw3H.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/mtOBulV.jpg" alt="Fears are nothing more than a state of mind" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/MEGFkfo.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/MEGFkfo.jpg" alt="A man looking strong" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                   </div>
@@ -192,13 +192,13 @@ class Fear extends Component {
                   <div className="row">
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                          <img src="https://imgur.com/Ci5pfBw.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                          <img src="https://imgur.com/au1gykS.jpg" alt="Feel the fear and do it anyway" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 Big mb-4">
                         <div className="card border border-white rounded card-img-top">
                         <a href="https://www.mentalhealth.org.uk/publications/overcome-fear-anxiety" target="_blank" rel="noopener noreferrer">
-                          <img src="https://imgur.com/1Wke4D1.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                          <img src="https://imgur.com/1Wke4D1.jpg" alt="How to overcome fear and anxiety" className="rounded card-img-top img-fluid"/>
                         </a>
                         </div>
                       </div>
@@ -214,7 +214,7 @@ class Fear extends Component {
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 Big mb-4">
                         <div className="card border border-white rounded card-img-top">
                         <a href="https://hadithoftheday.com/coping-with-fear/" target="_blank" rel="noopener noreferrer">
-                        <img src="https://imgur.com/OSdOrXz.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/OSdOrXz.jpg" alt="Coping with Fear" className="rounded card-img-top img-fluid"/>
                         </a>
                         </div>
                       </div>
@@ -225,7 +225,7 @@ class Fear extends Component {
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/6xhd5Bo.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/bUsSNoc.jpg" alt="Courage dear heart" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                   </div>
@@ -239,12 +239,12 @@ class Fear extends Component {
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/cvEQeGH.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/q5Ycded.jpg" alt="If you are not going all the way, why go at all" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                       <div className="col-12 col-sm-12 col-mb-4 col-lg-4 col-xl-4 mb-4">
                         <div className="card border border-white rounded card-img-top">
-                        <img src="https://imgur.com/abOMisX.jpg" alt="..." className="rounded card-img-top img-fluid"/>
+                        <img src="https://imgur.com/N7aNGUE.jpg" alt="never let your fear decide your future" className="rounded card-img-top img-fluid"/>
                         </div>
                       </div>
                   </div>

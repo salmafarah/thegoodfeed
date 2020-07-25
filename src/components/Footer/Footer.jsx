77d205e43c4cf6a17c56f1b51fd350608468a4e4
Salma Farah <salmafarah08@gmail.com>
2020-07-24 22:57:25 -0400

    updated footter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ class Footer extends Component {
     render(){
         return(
             <div className="navbar Footer"> 
-           <p> Footer</p>
+           <p> The Good Feed was created as a project for Ummahacks. None of the images or resources on the page belong to us and all credit is given to the content creators </p>
         </div> 
         );
     }

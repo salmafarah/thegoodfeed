a4b4d792decad4a688391542a673dc52549243e3
Salma Farah <salmafarah08@gmail.com>
2020-07-26 02:13:24 -0400

    Updates

---
+++ precache-manifest.dcbaa3c8995ff22633e4930dd0cf2102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52e5e3e7b66a72ea05ec3d85c5fe1dd",
+    "url": "/thegoodfeed/index.html"
+  },
+  {
+    "revision": "b632e4e6035973f2cc1e",
+    "url": "/thegoodfeed/static/css/main.61fef3aa.chunk.css"
+  },
+  {
+    "revision": "dd8365fe5d1db3c7663e",
+    "url": "/thegoodfeed/static/js/2.eeee31d8.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/thegoodfeed/static/js/2.eeee31d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b632e4e6035973f2cc1e",
+    "url": "/thegoodfeed/static/js/main.5bd43ee3.chunk.js"
+  },
+  {
+    "revision": "2996187a5edb2d2888c8",
+    "url": "/thegoodfeed/static/js/runtime-main.7b2c69cc.js"
+  }
+]);

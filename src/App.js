b350355d044e6068dd0fdddb5cae8477e4a99c35
LIFE <dkrshw65@gmail.com>
2020-08-22 05:46:25 -0400

    removed render={}

--- src/App.js
+++ src/App.js
@@ -1,80 +1,36 @@
-import React, {Component} from 'react';
-import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav'; 
-import Footer from './components/Footer/Footer';
-import HomePage from './components/HomePage/HomePage';
-import About from './pages/About/About';
-import Happy from './components/Happy/Happy';
-import Angry from './components/Anger/Anger';
-import Sad from './components/Sad/Sad';
-import Meh from './components/Meh/Meh';
-import Fear from './components/Fear/Fear';
-import Anxiety from './components/Anxiety/Anxiety';
+import React, { Component } from "react";
+import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./components/HomePage/HomePage";
+import About from "./pages/About/About";
+import Happy from "./components/Happy/Happy";
+import Angry from "./components/Anger/Anger";
+import Sad from "./components/Sad/Sad";
+import Meh from "./components/Meh/Meh";
+import Fear from "./components/Fear/Fear";
+import Anxiety from "./components/Anxiety/Anxiety";
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <HashRouter basename='/'>
+      <HashRouter basename="/">
         <div className="App">
-          <Nav/>            
-          <Route
-              exact path="/"
-                render={ props => (
-                  <HomePage/>
-                )}
-              />
-              <Route
-              path="/about"
-                render={ props => (
-                  <About/>
-                )}
-              />
-              <Route
-              path="/happy"
-                render={ props => (
-                  <Happy/>
-                )}
-              />
-              <Route
-              path="/sad"
-                render={ props => (
-                  <Sad/>
-                )}
-              />
-            <Route
-              path="/fear"
-                render={ props => (
-                  <Fear/>
-                )}
-              />
-            <Route
-              path="/meh"
-                render={ props => (
-                  <Meh/>
-                )}
-              />
-            <Route
-              path="/angry"
-                render={ props => (
-                  <Angry/>
-                )}
-              />
-              <Route
-              path="/anxiety"
-                render={ props => (
-                  <Anxiety/>
-                )}
-              />
-          <Footer/>
+          <Nav />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={About} />
+          <Route path="/happy" component={Happy} />
+          <Route path="/sad" component={Sad} />
+          <Route path="/fear" component={Fear} />
+          <Route path="/meh" component={Meh} />
+          <Route path="/angry" component={Angry} />
+          <Route path="/anxiety" component={Anxiety} />
+          <Footer />
         </div>
       </HashRouter>
     );
   }
 }
-
-
-
-
 
 export default App;
